9375bcab1198d2040eed7e8da7f6c86a61c91464
Ancientshield <jacklin2100@gmail.com>
2024-07-29 19:43:59 +0800

    使用@符號

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
-    <el-button type="primary" size="default">主要按鈕</el-button>
+    <el-button type="primary" size="default" :icon="Plus">主要按鈕</el-button>
+    <el-button type="success" size="small" :icon="Edit">編輯按鈕</el-button>
+    <el-button type="danger" size="default" :icon="Delete">刪除按鈕</el-button>
   </div>
+  <div>
+    <el-pagination background layout="prev, pager, next" :total="1000" />
+  </div>
+  <Test></Test>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 引入 element-plus 圖標元件
+import { Plus, Edit, Delete } from '@element-plus/icons-vue';
+import Test from '@/components/Test.vue';
+</script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import App from '@/App.vue';
 
 // 引用 element-plus 插件和樣式
 import ElementPlus from 'element-plus';
